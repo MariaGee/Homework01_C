51ba63cc2298b30680e8da7bbef766c4c2eb4170
MariaGee <masha.avramchencko@yandex.com>
2022-08-09 09:29:31 +0300

    Solution to task 08

---
+++ Task08/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+int numberA = new Random().Next(0, 100);
+int count = 1;
+
+Console.WriteLine("Было задано число: " + numberA);
+
+while(count <= numberA)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + " ");
+    }
+    count++;
+}
