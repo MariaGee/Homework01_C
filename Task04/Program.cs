337c7ed75809704020a25aa65dbc4f78cf7ffffe
MariaGee <masha.avramchencko@yandex.com>
2022-08-09 09:15:20 +0300

    Solution to task 04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -4,4 +4,15 @@ int numberB = new Random().Next(0, 100);
 int numberC = new Random().Next(0, 100);
 int max = numberA;
 
+Console.WriteLine("Были заданы числа: " + numberA + ", " + numberB + ", " + numberC);
 
+if (max < numberB)
+{
+    max = numberB;
+}
+else if (max < numberC)
+{
+    max = numberC;
+}
+
+Console.WriteLine("Самое большое число: " + max);
