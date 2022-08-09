86fc18402b3b8641931efc5353afa8a4d4d9c18f
MariaGee <masha.avramchencko@yandex.com>
2022-08-09 08:54:56 +0300

    Solution to task 02

---
+++ Task02/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+int numberA = new Random().Next(0, 100);
+int numberB = new Random().Next(0, 100);
+int max = numberA;
+
+Console.WriteLine("Были заданы следующие числа: " + numberA + " и " + numberB);
+
+if (max < numberB)
+{
+    max = numberB;
+    Console.Write("Число " + max + " больше числа " + numberA);
+}
+else
+{
+    Console.Write("Число " + max + " больше числа " + numberB);
+}
